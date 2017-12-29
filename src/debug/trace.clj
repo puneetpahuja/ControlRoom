766bb72410af2ef59feeec9e949459a8f0962c9a
Puneet Pahuja <er.puneetpahuja@gmail.com>
2017-12-29 15:48:09 +0530

    Adds tracing helper file

---
+++ src/debug/trace.clj
@@ -0,0 +1,14 @@
+(ns debug.trace
+  (:require [clojure.tools.trace :as t]))
+
+(defn trace [& ns-list]
+  (map #(t/trace-ns %) ns-list))
+
+(trace 'api.auth
+       'api.handler
+       'api.schema)
+
+(trace 'dtm.auth
+       'dtm.config
+       'dtm.convert
+       'dtm.util)
